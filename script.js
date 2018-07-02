5454cae6a9f52df51f6712b1da44d2568bbf310c
den4eGgpower <40765700+den4eGgpower@users.noreply.github.com>
2018-07-02 17:38:39 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -3,19 +3,30 @@ var close = document.querySelector('.close');
 var grey = document.querySelector('.dialog');
 var aside = document.querySelector('aside');
 var btn2 = document.querySelector('button.mgt')
-
+var phone = document.querySelector('.phone')
 
 close.addEventListener('click' , function (){
     grey.style.display = 'none';
 aside.style.display = 'none';
+
+btn2.style.display = 'block'
 })
 
 btn1.addEventListener('click' , function (){
     grey.style.display = 'block';
     aside.style.display = 'block';
+  
+btn2.style.display = 'none'
     })
 btn2.addEventListener('click' , function (){
     grey.style.display = 'block';
     aside.style.display = 'block';
+    btn2.style.display = 'none'
     })
+    phone.addEventListener('click' , function (){
+        grey.style.display = 'block';
+        aside.style.display = 'block';
+        btn2.style.display = 'none'
+        })
+
     
